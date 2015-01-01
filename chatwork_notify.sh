e2f9d3fe18e93683d0e774152bbcc2fe5b2e8a5e
kohei kinoshita <aozora0000@gmail.com>
2015-01-01 16:25:16 +0900

    fixed

--- chatwork_notify.sh
+++ chatwork_notify.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -e
 
 usage() {
-    cat << EOF
-    Usage: $0 -t <token>
+cat << EOF
+Usage: $0 -t <token>
 
-    This script will read from stdin and send the contents to the given room as
-    a system message.
+This script will read from stdin and send the contents to the given room as
+a system message.
 
-    OPTIONS:
-    -h Show this message
-    -t <token> API token
-    -r room id
-    -o API host (api.chatwork.com)
-    EOF
+OPTIONS:
+-h Show this message
+-t <token> API token
+-r room id
+-o API host (api.chatwork.com)
+EOF
 }
 
 TOKEN=${CHATWORK_TOKEN:-}
