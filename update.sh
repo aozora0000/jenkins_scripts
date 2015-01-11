8076be698422d6ecf756bee240b80967bfa00f7e
kohei kinoshita <aozora0000@gmail.com>
2015-01-11 12:34:32 +0900

    add docker test builder

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ HIPCHAT_NOTIFY=https://raw.githubusercontent.com/aozora0000/jenkins_scripts/mast
 IDOBATA_NOTIFY=https://raw.githubusercontent.com/aozora0000/jenkins_scripts/master/idobata_notify.sh
 IRC_NOTIFY=https://raw.githubusercontent.com/aozora0000/jenkins_scripts/master/irc_notify.sh
 CHATWORK_NOTIFY=https://raw.githubusercontent.com/aozora0000/jenkins_scripts/master/chatwork_notify.sh
-
+DOCKER_TEST_BUILDER=https://raw.githubusercontent.com/aozora0000/jenkins_scripts/master/dockertestbuilder.sh
 
 CURL_OPTION="-# --retry 3 --retry-delay 4 -L -o"
 
@@ -24,6 +24,7 @@ echo_c $BLUE "
 #############################################
 # UpdateHistory
 #############################################
+rev4 2015-01-11 add dockertestbuilder(not use .jenkins.yml ver)
 rev3 2015-01-01 requirement Jq command & chatwork-notify Deployment
 rev2 2014-12-26 irc-notify Deployment
 rev1 2014-12-26 hipchat-notify & idobata-notify & yaml-parser Deployments
@@ -47,7 +48,8 @@ rm -f /usr/bin/yaml-parser     && curl $CURL_OPTION /usr/bin/yaml-parser     $YA
 rm -f /usr/bin/hipchat-notify  && curl $CURL_OPTION /usr/bin/hipchat-notify  $HIPCHAT_NOTIFY  && chmod 0777 /usr/bin/hipchat-notify  && \
 rm -f /usr/bin/idobata-notify  && curl $CURL_OPTION /usr/bin/idobata-notify  $IDOBATA_NOTIFY  && chmod 0777 /usr/bin/idobata-notify  && \
 rm -f /usr/bin/irc-notify      && curl $CURL_OPTION /usr/bin/irc-notify      $IRC_NOTIFY      && chmod 0777 /usr/bin/irc-notify      && \
-rm -f /usr/bin/chatwork-notify && curl $CURL_OPTION /usr/bin/chatwork-notify $CHATWORK_NOTIFY && chmod 0777 /usr/bin/chatwork-notify
+rm -f /usr/bin/chatwork-notify && curl $CURL_OPTION /usr/bin/chatwork-notify $CHATWORK_NOTIFY && chmod 0777 /usr/bin/chatwork-notify && \
+rm -f /usr/bin/dockertestbuilder && curl $CURL_OPTION /usr/bin/dockertestbuilder $CHATWORK_NOTIFY && chmod 0777 /usr/bin/dockertestbuilder
 
 echo_c $GREEN "
 #############################################
